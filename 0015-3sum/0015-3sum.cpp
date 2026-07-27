976bb039f84ba914bd9916a3aa653cf0670c16a5
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-07-27 18:16:29 +0530

    Time: 43 ms (83.77%), Space: 29.2 MB (47.44%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,36 +5,26 @@ public:
         vector<vector<int>> result;
 
         sort(nums.begin(), nums.end());
-
-        for(int i =0; i< n-2; i++){
-            int left = i+1;
-            int right = n-1;
-    
-            if(i > 0 && nums[i] == nums[i-1])
+        for(int i =0; i < n - 2 ; i++){  // no need to iterate when fewer than 3 elements 
+            if(i >0 && nums[i] == nums[i-1]){
                 continue;
-            
-            while(left < right){
-                int sum = nums[i]+ nums[left] + nums[right];
-
-                if(sum == 0){
-                    result.push_back({nums[i], nums[left], nums[right]});
-
-                    while(left < right && nums[left] == nums[left+1])
-                        left++;
-                    
-                    while(left < right && nums[right] == nums[right-1])
-                        right--;
-
-                    left++;
-                    right--;
-                }
-                else if(sum < 0){
-                    left++;
-                }
-                else{
-                    right--;
-                }
             }
+            int j = i+1;
+            int k = n - 1;
+             while(j < k ){
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum < 0) j++;
+                else if (sum > 0) k--;
+                else{
+                    result.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
+
+                }
+             }
         }
         return result;
     }
