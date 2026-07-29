1f63ae308aac7559fee0acb18e8c8724ebd021f8
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-07-29 19:41:40 +0530

    Update 0904-fruit-into-baskets.cpp

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        int maxLen = 0;
+        int n = fruits.size();
+
+        for (int i = 0; i < n; i++) {
+            set<int> st;
+            for (int j = i; j < n; j++) {
+                st.insert(fruits[j]);
+                if (st.size() <= 2) {
+                    maxLen = max(j - i + 1, maxLen);
+                } else {
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
+};
+
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
 
         int maxLen = 0;
         int n = fruits.size();
