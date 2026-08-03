7527b83afa479c3533cd5938fdf03882ccd24599
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-08-03 08:56:09 +0530

    Update 0523-continuous-subarray-sum.cpp

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -26,3 +26,55 @@ public:
         return false;
     }
 };
+
+
+
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        for(int i =0; i < n; i ++){
+            int sum = 0;
+            for(int j = i; j <n; j++){
+                sum += nums[j];
+
+                if((j-i+1) >= 2 && sum%k == 0)
+                    return true;
+            }
+        }
+        return false;
+    }
+};                             
+
+
+
+// not valid for 0 or -ve values class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+
+        int sum = 0;
+        mp[0] = -1;    // remainder 0 is first seen before the start of the array
+
+        for(int i =0; i < n; i++){
+            sum += nums[i];
+
+            if(k != 0)
+                sum %= k;
+
+            if(mp.count(sum)){
+                if(i - sum > 1){
+                    return true;
+                }
+            }
+            else{
+                    mp[sum] = i;
+                }
+        }
+        
+        return false;
+    }
+};
+
