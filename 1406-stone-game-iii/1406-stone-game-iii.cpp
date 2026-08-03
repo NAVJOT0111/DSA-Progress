af0bcff9c80277545270f7a691ae19f7b01a4e34
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-08-03 14:18:12 +0530

    Update 1406-stone-game-iii.cpp

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,3 +1,69 @@
+// it'll give TLE
+
+class Solution {
+public:
+
+    int solve(vector<int>& stoneValue, int i){
+        int n = stoneValue.size();
+
+        vector<int> t;   // instead of starting the recursive funct again everytime, store the values in a dp array
+        if(i >= n) return 0;
+
+        if(t[i] != -1)
+            return t[i];
+
+        int result = stoneValue[i] - solve(stoneValue, i+1);
+
+        if(i+1 < n)
+            result = max(result, stoneValue[i] + stoneValue[i+1] - solve(stoneValue, i+2));
+        if(i+2 < n)
+            result = max(result, stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - solve(stoneValue, i+3));
+
+        return return t[i] = result;
+    }
+
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        t.resize(n+1, -1); 
+
+        int diff = solve(stoneValue,0);
+
+        if(diff < 0)
+            return "Bob";
+        else if(diff > 0)
+            return "Alice";
+        return "Tie";
+    }
+};                                                       
+
+
+
+// O(n) time and space
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        vector<int> t(n+1);
+
+        for(int i = n -1; i >= 0; i--){
+
+            t[i] = stoneValue[i] - t[i+1];
+
+            if(i+2 <= n)
+                t[i] = max(t[i], stoneValue[i] + stoneValue[i+1] - t[i+2]);
+            if(i+3 <= n)
+                t[i] = max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - t[i+3]);
+        }
+
+        int diff = t[0];
+
+        if(diff < 0)
+            return "Bob";
+        else if(diff > 0)
+            return "Alice";
+        return "Tie";
+    }
+};
 
 
 
