cd340257228ecba0423efe6d6c0bb3ba87753c9f
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-08-05 18:36:06 +0530

    Update 1749-maximum-absolute-sum-of-any-subarray.cpp

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,3 +1,21 @@
+//https://chatgpt.com/c/6a73313f-6468-83e8-a498-3c8a7a01d553
+class Solution {
+public:
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n = nums.size();
+        int maxSum = nums[0];
+
+        for(int i =0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                maxSum = max(maxSum, abs(sum));
+            }
+        }
+        return maxSum;
+    }
+};
+
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
