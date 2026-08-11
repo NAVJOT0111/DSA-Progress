13ba45e3501573011cf56a824cf81aef6eb7eeb6
N_Kaur <189604130+NAVJOT0111@users.noreply.github.com>
2026-08-11 09:04:52 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.24%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+       if(x == 0 || x == 1)
+        return x;
+
+        int low = 1;
+        int high = x;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if((long) mid*mid > (long) x)
+                high = mid - 1;
+            else if(mid *mid == x)
+                return mid;
+            else
+                low = mid + 1;
+        }
+        return high;
+    }
+};
